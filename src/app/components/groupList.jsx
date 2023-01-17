dfa270ae697771b682a290544a8f8dbd6e895e88
JacobWestfield <zzultekk@gmail.com>
2023-01-17 23:12:26 +0300

    patch

---
+++ src/app/components/groupList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const GroupList = ({ items }) => {
+    return (
+        <ul className="list-group">
+            {Object.keys(items).map((item) => (
+                <li key={items[item]._id} className="list-group-item">
+                    {items[item].name}
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+GroupList.propTypes = {
+    items: PropTypes.object.isRequired
+};
+export default GroupList;
